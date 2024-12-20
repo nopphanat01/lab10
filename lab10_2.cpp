35232de1d9998dc2a43baee77a1fe89bbec6b407
nopphanat01 <sspas401@gmail.com>
2024-12-20 16:13:33 +0700

    lab10

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,9 +6,17 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
+	dest << "-------------------- BOOM ---------------------" << endl;
+
+	string line;
+	while (getline(source, line)) {
+		dest << line << endl;
+	}
+
+	dest << "-------------------- HA!! ---------------------" << endl;
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,33 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    int count = 0;
+    float sum = 0.0;
+    float sum_of_square = 0.0;
+    string textline;
+
+    ifstream source("score.txt");
+
+    while (getline(source, textline)) {
+        float value = stof(textline);
+        sum += value;
+        sum_of_square += value * value;
+        count++;
+    }
+
+    float mean = sum / count;
+    float std_dev = sqrt((sum_of_square / count) - (mean * mean));
+
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << std_dev << "\n";
+
+    return 0;
+}
